056c6b867173e910d3a26ad7cffaa1278b1a3f87
Dubinets A.V <dubinets.av@gmail.com>
2018-10-08 00:38:16 +0300

    cmake 3.6.2

--- test/basic_test001.cpp
+++ test/basic_test001.cpp
@@ -4,6 +4,18 @@
 
 #include <gtest/gtest.h>
 
-TEST(Basic_tests, Test001) {
+TEST(Basic_tests, TestEq_1_1) {
+    EXPECT_EQ(1, 1);
+}
 
+TEST(Basic_tests, TestEq_2_2) {
+    EXPECT_EQ(1, 1);
+}
+
+TEST(Basic_tests, TestNe_2_1) {
+    EXPECT_EQ(2, 1);
+}
+
+TEST(Basic_tests, TestNe_1_2) {
+    EXPECT_EQ(1, 2);
 }
